2ed590c09a63e5305ec38d22d03f1a1689bae834
Daksh Sharma <daksh.sharma.ds11@gmail.com>
2026-08-09 13:28:54 +0100

    Adding Project to remote git

---
+++ javaScriptLearning/dataType.js
@@ -0,0 +1,30 @@
+/*=============Data Types in JavaScript=================
+There are 2 types of Data types in JavaScript
+1. Primitive Data Types
+2. Non-Primitive Data Types
+1. Primitive Data Types: These are the basic data types in JavaScript. They include:
+   - Number: Represents numeric values (e.g., 1, 2, 3.14).
+   - String: Represents sequences of characters (e.g., "Hello", 'World').
+   - Boolean: Represents true or false values.
+   - Undefined: Represents a variable that has been declared but has not been assigned a value.
+   - Null: Represents the intentional absence of any object value.
+   ------------we dont use these below data type in automation-----------
+   - Symbol: Represents a unique and immutable value (introduced in ES6).
+   - BigInt: Represents integers with arbitrary precision (introduced in ES2020).
+2. Non-Primitive Data Types: These are more complex data types that can hold multiple values or objects. They include:
+   - Object: Represents a collection of key-value pairs (e.g., { name: "John", age: 30 }).
+   - Array: Represents an ordered list of values (e.g., [1, 2, 3, 4]).
+   - Function: Represents a reusable block of code that can be called with arguments.*/
+
+let num = 1;
+let name = "My name is Nidhi";
+let booleanValue = true;
+console.log(num);
+let a= 1;
+console.log("a value is: " + a );
+let array = new Array("pune", "mumbai", "banglore");
+console.log(array.length);
+for(let i in array)
+    {
+        console.log(i);
+    }
