e5737444358cb52e6e6952a11357363cd019dcfd
Daksh Sharma <daksh.sharma.ds11@gmail.com>
2026-08-09 20:20:25 +0100

    data type

--- javaScriptLearning/dataType.js
+++ javaScriptLearning/dataType.js
@@ -15,9 +15,33 @@ There are 2 types of Data types in JavaScript
    - Object: Represents a collection of key-value pairs (e.g., { name: "John", age: 30 }).
    - Array: Represents an ordered list of values (e.g., [1, 2, 3, 4]).
    - Function: Represents a reusable block of code that can be called with arguments.*/
+console.log("-------------------Number example -------------");
 
 let num = 1;
-let name = "My name is Nidhi";
+console.log("num value is: " + num);
+console.log(typeof num);
+
+
+let num1 = 45.67;
+console.log("num1 value is: " + num1);
+console.log(typeof num1);
+
+
+console.log("-------------------String example -------------");
+let name = "Nidhi";
+console.log("name value is: " + name);
+console.log(typeof name);
+
+let name1 = 'I work in Aztec';
+console.log("name1 value is: " + name1);
+console.log(typeof name1);
+
+let name2 = `String type 3 way`;
+console.log("name2 value is: " + name2);
+console.log(typeof name2);
+
+
+/*let name = "My name is Nidhi";
 let booleanValue = true;
 console.log(num);
 let a= 1;
@@ -28,3 +52,4 @@ for(let i in array)
     {
         console.log(i);
     }
+*/
